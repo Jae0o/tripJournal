4440d5239948f16388fe1c0d1269a90479bed9b5
Lee Jae Yeong <leey153@naver.com>
2023-08-21 20:52:55 +0900

    make Country Card

--- src/Pages/Korea.jsx
+++ src/Pages/Korea.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../CSS/Map/Asia/Korea/Korea.css";
 import KoreaMap from '../Components/Map/Asia/Korea/KoreaMap';
+import CountryCard from '../Components/CountryCard';
 
 export default function Korea() {
 
@@ -8,7 +9,12 @@ export default function Korea() {
   return (
     <div className='KoreaPage'>
       <div className='KoreaPage_left'>
-
+        <CountryCard
+          title={"KOR"}
+          capital={"Seoul City"}
+          info={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, fugit perspiciatis. Ad, error nulla nemo perspiciatis aliquam repellat laborum provident, aspernatur suscipit quibusdam quasi odit?"}
+          img={'/Img/Korea.jpg'}
+        />
 
       </div>
       <div className='KoreaPage_right'>
